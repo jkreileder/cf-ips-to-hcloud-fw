571d0cf8d672a3a227c28d8357234af44024c8a6
Jürgen Kreileder <jk@blackdown.de>
2024-07-15 14:55:59 +0200

    Bump cloudflare from 2.20.0 to 3.0.1 (#426)
    Adapt code and tests
    
    Signed-off-by: Jürgen Kreileder <jk@blackdown.de>

--- tests/test_cloudflare.py
+++ tests/test_cloudflare.py
@@ -2,33 +2,78 @@ from __future__ import annotations
 
 from unittest.mock import MagicMock, patch
 
-import CloudFlare  # type: ignore[import-untyped]
+import cloudflare
+import cloudflare.types.ips
+import httpx
 import pytest
 
 from cf_ips_to_hcloud_fw.cloudflare import (
-    cf_ips_get,  # type: ignore[import-untyped]
+    cf_ips_list,
     get_cloudflare_cidrs,
 )
 from cf_ips_to_hcloud_fw.models import CloudflareCIDRs
 
 
-@patch("CloudFlare.CloudFlare")
+@patch("cloudflare.Cloudflare")
 @patch("logging.error")
-def test_cf_ips_get(mock_logging: MagicMock, mock_cloudflare: MagicMock) -> None:
-    mock_cloudflare.return_value.ips.get.side_effect = (
-        CloudFlare.exceptions.CloudFlareAPIError(503, "503")  # type: ignore[assignment]
+def test_cf_ips_list_api_connection_error(
+    mock_logging: MagicMock, mock_cloudflare: MagicMock
+) -> None:
+    mock_cloudflare.return_value.ips.list.side_effect = cloudflare.APITimeoutError(
+        httpx.Request("GET", "https://api.cloudflare.com/client/v4/ips")
     )
     with pytest.raises(SystemExit) as e:
-        cf_ips_get()
+        cf_ips_list()
     assert e.type is SystemExit
     assert e.value.code == 1
-    mock_cloudflare.return_value.ips.get.assert_called_once()
-    mock_logging.assert_called_once_with("Error getting CloudFlare IPs: 503")
+    mock_cloudflare.return_value.ips.list.assert_called_once()
+    mock_logging.assert_called_once_with(
+        "Error getting CloudFlare IPs: Request timed out."
+    )
+
+
+@patch("cloudflare.Cloudflare")
+@patch("logging.error")
+def test_cf_ips_list_api_status_error(
+    mock_logging: MagicMock, mock_cloudflare: MagicMock
+) -> None:
+    mock_cloudflare.return_value.ips.list.side_effect = cloudflare.RateLimitError(
+        "rate-limit",
+        response=httpx.Response(
+            status_code=200,
+            request=httpx.Request("GET", "https://api.cloudflare.com/client/v4/ips"),
+        ),
+        body=None,
+    )
+    with pytest.raises(SystemExit) as e:
+        cf_ips_list()
+    assert e.type is SystemExit
+    assert e.value.code == 1
+    mock_cloudflare.return_value.ips.list.assert_called_once()
+    mock_logging.assert_called_once_with("Error getting CloudFlare IPs: rate-limit")
 
 
 @patch(
-    "cf_ips_to_hcloud_fw.cloudflare.cf_ips_get",
-    MagicMock(return_value={"ipv4_cidrs": ["199.27.128.0/21", "198.27.128.0/21"]}),
+    "cf_ips_to_hcloud_fw.cloudflare.cf_ips_list",
+    MagicMock(return_value=None),
+)
+@patch("logging.error")
+def test_get_cloudflare_cidrs_no_response(mock_logging: MagicMock) -> None:
+    with pytest.raises(SystemExit) as e:
+        get_cloudflare_cidrs()
+    assert e.type is SystemExit
+    assert e.value.code == 1
+    mock_logging.assert_called_once_with("Cloudflare/ips.list: no response")
+
+
+@patch(
+    "cf_ips_to_hcloud_fw.cloudflare.cf_ips_list",
+    MagicMock(
+        return_value=cloudflare.types.ips.IPs(
+            ipv4_cidrs=["399.27.128.0/21", "198.27.128.0/21"],
+            ipv6_cidrs=["2400:cb00::/32", "1400:cb00::/32"],
+        )
+    ),
 )
 @patch("logging.error")
 def test_get_cloudflare_cidrs_invalid(mock_logging: MagicMock) -> None:
@@ -37,16 +82,16 @@ def test_get_cloudflare_cidrs_invalid(mock_logging: MagicMock) -> None:
     assert e.type is SystemExit
     assert e.value.code == 1
     mock_logging.assert_called_once()
-    assert "Cloudflare/ips.get didn't validate" in mock_logging.call_args[0][0]
+    assert "Cloudflare/ips.list didn't validate" in mock_logging.call_args[0][0]
 
 
 @patch(
-    "cf_ips_to_hcloud_fw.cloudflare.cf_ips_get",
+    "cf_ips_to_hcloud_fw.cloudflare.cf_ips_list",
     MagicMock(
-        return_value={
-            "ipv4_cidrs": ["199.27.128.0/21", "198.27.128.0/21"],
-            "ipv6_cidrs": ["2400:cb00::/32", "1400:cb00::/32"],
-        }
+        return_value=cloudflare.types.ips.IPs(
+            ipv4_cidrs=["199.27.128.0/21", "198.27.128.0/21"],
+            ipv6_cidrs=["2400:cb00::/32", "1400:cb00::/32"],
+        )
     ),
 )
 def test_get_cloudflare_cidrs() -> None:
