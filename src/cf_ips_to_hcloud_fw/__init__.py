67d9579aaae1aa4cf1eaaaca33b950a29053bf6c
Jürgen Kreileder <jk@blackdown.de>
2025-06-27 13:47:16 +0200

    Bump ruff version to 0.12.1 and remove noqa comment from version import
    Signed-off-by: Jürgen Kreileder <jk@blackdown.de>

--- src/cf_ips_to_hcloud_fw/__init__.py
+++ src/cf_ips_to_hcloud_fw/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import annotations
 
-from cf_ips_to_hcloud_fw.version import __VERSION__ as __VERSION__  # noqa: PLC0414
+from cf_ips_to_hcloud_fw.version import __VERSION__ as __VERSION__
